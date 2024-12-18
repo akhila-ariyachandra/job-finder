b578c4c92105b67ec493cc08d967e34f003a9b61
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2024-12-18 12:33:58 +0530

    Added option for deployed API

--- apps/web/app/_lib/api.ts
+++ apps/web/app/_lib/api.ts
@@ -1,4 +1,6 @@
 import { hc } from "hono/client";
 import type { AppType } from "server";
 
-export const client = hc<AppType>("http://localhost:4000");
+export const client = hc<AppType>(
+  process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:4000",
+);
